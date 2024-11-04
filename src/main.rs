bfa66a44beec5b79a49a22cfa7aedf55f7b7e1f0
Garrett Moseke <gmosedev@proton.me>
2024-11-04 21:13:55 +0100

    refactor: camera follows player
    mostly yoinked from the bevy examples here:
    https://bevyengine.org/examples/camera/2d-top-down-camera/

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use bevy::prelude::*;
 
+const CAM_LERP_FACTOR: f32 = 2.;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, sprite_movement)
+        .add_systems(Update, (move_player, update_camera).chain())
         .run();
 }
 
@@ -18,29 +20,70 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         SpriteBundle {
             texture: asset_server.load("crab.png"),
-            transform: Transform::from_scale(Vec3::splat(0.05) ),
+            transform: Transform::from_scale(Vec3::splat(0.05)),
             ..default()
         },
         Player { speed: 200.0 },
     ));
 }
 
-fn sprite_movement(
+/// Update the camera position by tracking the player.
+fn update_camera(
+    mut camera: Query<&mut Transform, (With<Camera2d>, Without<Player>)>,
+    player: Query<&Transform, (With<Player>, Without<Camera2d>)>,
     time: Res<Time>,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut sprite_position: Query<(&mut Player, &mut Transform)>,
 ) {
-    let (player, mut transform) = sprite_position.single_mut();
-    if keyboard_input.pressed(KeyCode::ArrowRight) {
-        transform.translation.x += player.speed * time.delta_seconds();
-    }
-    if keyboard_input.pressed(KeyCode::ArrowLeft) {
-        transform.translation.x -= player.speed * time.delta_seconds();
-    }
-    if keyboard_input.pressed(KeyCode::ArrowUp) {
-        transform.translation.y += player.speed * time.delta_seconds();
-    }
-    if keyboard_input.pressed(KeyCode::ArrowDown) {
-        transform.translation.y -= player.speed * time.delta_seconds();
-    }
+    let Ok(mut camera) = camera.get_single_mut() else {
+        return;
+    };
+
+    let Ok(player) = player.get_single() else {
+        return;
+    };
+
+    let Vec3 { x, y, .. } = player.translation;
+    let direction = Vec3::new(x, y, camera.translation.z);
+
+    // Applies a smooth effect to camera movement using interpolation between
+    // the camera position and the player position on the x and y axes.
+    // Here we use the in-game time, to get the elapsed time (in seconds)
+    // since the previous update. This avoids jittery movement when tracking
+    // the player.
+    camera.translation = camera
+        .translation
+        .lerp(direction, time.delta_seconds() * CAM_LERP_FACTOR);
 }
+
+/// Update the player position with keyboard inputs.
+fn move_player(
+    mut player_info: Query<(&mut Player, &mut Transform)>,
+    time: Res<Time>,
+    kb_input: Res<ButtonInput<KeyCode>>,
+) {
+    let (player, mut player_transform) = player_info.single_mut();
+
+    let mut direction = Vec2::ZERO;
+
+    if kb_input.pressed(KeyCode::KeyW) {
+        direction.y += 1.;
+    }
+
+    if kb_input.pressed(KeyCode::KeyS) {
+        direction.y -= 1.;
+    }
+
+    if kb_input.pressed(KeyCode::KeyA) {
+        direction.x -= 1.;
+    }
+
+    if kb_input.pressed(KeyCode::KeyD) {
+        direction.x += 1.;
+    }
+
+    // Progressively update the player's position over time. Normalize the
+    // direction vector to prevent it from exceeding a magnitude of 1 when
+    // moving diagonally.
+    let move_delta = direction.normalize_or_zero() * player.speed * time.delta_seconds();
+    player_transform.translation += move_delta.extend(0.);
+}
+
